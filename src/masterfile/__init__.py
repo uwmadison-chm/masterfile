82217bab5263984c3507a68c1b94f9d315bafc63
Nate Vack <njvack@wisc.edu>
2017-08-28 13:13:42 -0500

    Add __package_version__ for version description
    Example: "masterfile 0.1.0dev"

--- src/masterfile/__init__.py
+++ src/masterfile/__init__.py
@@ -3,3 +3,5 @@
 from __future__ import absolute_import
 
 from ._metadata import version as __version__, author as __author__, email as __email__
+
+__package_version__ = 'masterfile {}'.format(__version__)
