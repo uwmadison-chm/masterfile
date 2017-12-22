dfd0d41098d9d55c052a55ae542bf585db260952
Nate Vack <njvack@wisc.edu>
2017-12-22 11:28:19 -0600

    Add number-to-Excel-column-index function
    Does what it says on the tin -- this will be useful for showing warning
    messages when validating the masterfile data.

---
+++ src/masterfile/formatters.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Part of the masterfile package: https://github.com/njvack/masterfile
+# Copyright (c) 2017 Board of Regents of the University of Wisconsin System
+# Written by Nate Vack <njvack@wisc.edu> at the Center for Healthy Minds
+# at the University of Wisconsin-Madison.
+# Released under MIT licence; see LICENSE at the package root.
+
+"""
+This package contains functions for pretty-printing data — for example,
+converting column numbers into Excel-like column identifier strings.
+"""
+
+import string
+
+
+def index_to_column_id(number):
+    """
+    Takes a zero-based index and converts it to a column identifier string
+    such as used in Excel. Examples:
+    0 => A
+    25 => Z
+    26 => AA
+    """
+    if number < 0 or not isinstance(number, int):
+        raise AttributeError("index_to_column_id requires a non-negative int")
+    digits = string.ascii_uppercase
+    if number == 0:
+        return digits[0]
+    return __rec_int_to_colid(number, digits)
+
+
+def __rec_int_to_colid(number, digits):
+    base = len(digits)
+    if number < 0:
+        return ''
+
+    return (
+        __rec_int_to_colid((number // base) - 1, digits) +
+        digits[number % base])
---
+++ tests/test_formatters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Part of the masterfile package: https://github.com/njvack/masterfile
+# Copyright (c) 2017 Board of Regents of the University of Wisconsin System
+# Written by Nate Vack <njvack@wisc.edu> at the Center for Healthy Minds
+# at the University of Wisconsin-Madison.
+# Released under MIT licence; see LICENSE at the package root.
+
+from masterfile.formatters import index_to_column_id
+
+import pytest
+
+
+class TestFormatters(object):
+
+    def test_index_to_column_id_normal_inputs(self):
+        assert index_to_column_id(0) == 'A'
+        assert index_to_column_id(25) == 'Z'
+        assert index_to_column_id(26) == 'AA'
+
+    def test_index_to_column_id_bad_inputs(self):
+        with pytest.raises(AttributeError):
+            index_to_column_id('A')
+        with pytest.raises(AttributeError):
+            index_to_column_id(1.1)
+        with pytest.raises(AttributeError):
+            index_to_column_id(-1)
