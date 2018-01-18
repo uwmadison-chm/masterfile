8e623faa44ad767baf4c92596a2501f98dfd2bbb
Nate Vack <njvack@wisc.edu>
2018-01-18 14:02:11 -0600

    Clean up validator package imports

--- src/masterfile/validators/__init__.py
+++ src/masterfile/validators/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
-from . import io_validator
-from . import index_column_validator
-
+from . import (  # noqa
+    io_validator,
+    index_column_validator
+)
