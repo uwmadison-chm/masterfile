60811bc4efabd0f2a9a1643d91ef96e7fee2bd99
Nate Vack <njvack@wisc.edu>
2018-01-18 10:14:32 -0600

    Improve help text, add exit code

--- src/masterfile/scripts/validate_masterfile.py
+++ src/masterfile/scripts/validate_masterfile.py
@@ -7,7 +7,7 @@
 # at the University of Wisconsin-Madison.
 # Released under MIT licence; see LICENSE at the package root.
 
-""" Validate masterfile data, dictionaries, and exclusion files.
+"""Validate masterfile data, dictionaries, and exclusion files.
 
 Usage: validate_masterfile [options] <masterfile_path>
 
@@ -26,11 +26,15 @@ import masterfile
 from masterfile import validator
 from masterfile.vendor.docopt import docopt
 
+import sys
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+CLEAN = 0
+ERRORS = 1
 
 
 def main(argv=None):
@@ -42,7 +46,10 @@ def main(argv=None):
     errors = validator.run_all_validators(mf)
     for e in errors:
         print(e)
+    if errors:
+        return ERRORS
+    return CLEAN
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
