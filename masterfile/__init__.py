db4be22e1eee7265e39c738f465c1a00312b9eeb
Nate Vack <njvack@wisc.edu>
2020-05-21 09:38:57 -0500

    Quiet flake8

--- masterfile/__init__.py
+++ masterfile/__init__.py
@@ -2,7 +2,11 @@
 
 from __future__ import absolute_import
 
-from ._metadata import version as __version__, author as __author__, email as __email__
+from ._metadata import (  # noqa: F401
+    version as __version__,
+    author as __author__,
+    email as __email__
+)
 
 __package_version__ = 'masterfile {}'.format(__version__)
 
